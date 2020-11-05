96b5f8f35ba59d3f5e6b0bc907449553a046ed39
Quynh Duyen <quynhduyen396@gmail.com>
2020-11-05 21:41:54 +0700

    Change Lap_Looping_VeTamGiac

--- Lab_Looping_VeTamGiac/index.php
+++ Lab_Looping_VeTamGiac/index.php
@@ -8,17 +8,106 @@
 </head>
 <body>
     <?php 
-
+        $n=6;
+        $flat = true;
+        $result = "";
         if (isset($_GET["loop"])) {
             # code...
             $url = $_SERVER["REQUEST_URI"];
             // echo "Đây là URL hiện tại của bạn: http://".$_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
-            echo $url;
-            list($chuoi1, $chuoi2) = explode("?",$url);
-            echo $chuoi2;
+            list($local, $ur) = explode("?",$url);
+            list($loop,$number,$types) = explode("&",$ur);
+            list($type) = explode(".", $types);
+            if (isset($_GET["number"])) {
+                # Xử lý chức năng vẽ tam giác
+                $n = $_GET["number"];
+                if (is_numeric($n)) {
+                    switch ($_GET["loop"]) {
+                        //Vòng lặp for
+                        case "for":
+                            if ($type == "type1") {
+                                for ($i=1; $i <= $n; $i++) { 
+                                    $result.= str_repeat("*", $i) . "<br />";
+                                }
+                            }
+                            if ($type == "type2") {
+                                for ($i=$n; $i >=1 ; $i--) { 
+                                    $result.= str_repeat("*", $i) . "<br />";
+                                }
+                            }
+                            if ($type =="type3") {
+                                for ($i=1; $i <=$n ; $i++) { 
+                                    $space = str_repeat("&nbsp;&nbsp;", $n - $i);
+                                    $character = str_repeat("*", 2*$i-1);
+                                    $result .= $space . $character.'<br />';
+                                }
+                            }
+
+                            //Vòng lặp while
+                            case "while":
+                                if ($type == "type1") {
+                                    $i = 1;
+                                    while ($i <= $n){
+                                        $result.= str_repeat("*", $i) . "<br />";
+                                        $i++;
+                                    }
+                                }
+                                if ($type == "type2") {
+                                    $i=$n;
+                                    while ($i >= 1){
+                                        $result.= str_repeat("*", $i) . "<br />";
+                                        $i--;
+                                    }
+                                }
+                                if ($type =="type3") {
+                                    $i=1;
+                                    while ($i <= $n){
+                                        $space = str_repeat("&nbsp;&nbsp;", $n - $i);
+                                        $character = str_repeat("*", 2*$i-1); 
+                                        $result .= $space . $character.'<br />';
+                                        $i++;
+                                    }
+                                }
+                                break;
+
+                            //Vòng lặp do..while
+                            case "do..while":
+                                if ($type == "type1") {
+                                    $i = 1;
+                                    do {
+                                        $result.= str_repeat("*", $i) . "<br />";
+                                        $i++;
+                                     }
+                                     while ($i <= $n);
+                                }
+
+                                if ($type == "type2") {
+                                    $i=$n;
+                                    do {
+                                        $result.= str_repeat("*", $i) . "<br />";
+                                        $i--;
+                                    } while ($i >= 1);
+                                }
+                                if ($type =="type3") {
+                                    $i=1;
+                                   do {
+                                        $space = str_repeat("&nbsp;&nbsp;", $n - $i);
+                                        $character = str_repeat("*", 2*$i-1);
+                                        $result .= $space . $character.'<br />';
+                                        $i++;
+                                   } while ($i <= $n);
+                                }
+                        default:
+                            
+                            break;
+                    }
+                } else {
+                    $flat = false;
+                }
+            }
         }
         
-        ?>
+    ?>
     <div class="content">
         <h1>In Tam Giác</h1>
         <form method="get" action="">
@@ -37,26 +126,26 @@
             </div>
             <div class="row" style="margin-top: 10px;">
                 <label>Số hàng: </label>
-                <input type="text" placeholder="6" name="number" value="6">
+                <input type="text" placeholder="6" name="number" value="<?php echo $n;?>">
             </div>
             <h5>Chọn mẫu tam giác:</h5>
             <div class="row">
                 <div class="type">
-                    <input type="image" name="type1" value="type1" src="images/tamgiac1.jpg">
+                    <input type="image" name="type1" value="type" src="images/tamgiac1.jpg">
                     <input style="margin: 0px 40px;" type="image" name="type2" value="type2" src="images/tamgiac2.jpg">
                     <input type="image" name="type3" value="type3" src="images/tamgiac3.jpg">
                   
                 </div>
             </div>
-            <!-- <div class="row"">
-                <input type="submit" name="submit" value="Xem kết quả">
-            </div> -->
         </form>
         <div class="result">
-            <!-- <img src="images/aries.jpg" alt="aries"/>
-            <p>Cung Ma Kết <span>(Aries: 21/3 – 20/4)</span></p> -->
+            <p>Kết quả</p>
+            <h4>
+                <?php 
+                    echo $result;
+                ?>
+            </h4>
         </div>
-        
     </div>
 </body>
 </html>
